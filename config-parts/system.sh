9744a8f2bbe10c7ec8194f08b337a9fbd3f68549
Eko Purwanto <eko.purwanto@monosense.io>
2023-10-22 18:30:17 +0700

    fix: fw config

--- config-parts/system.sh
+++ config-parts/system.sh
@@ -9,11 +9,13 @@ set system name-server '203.142.82.222'
 set system name-server '203.142.84.222'
 set system time-zone 'Asia/Jakarta'
 
+set system sysctl parameter kernel.pty.max value '24000'
+
 set system login user vyos authentication public-keys monosense-okd key "${SECRET_ADMIN_SSH_KEY1}"
 set system login user vyos authentication public-keys macbook key "${SECRET_ADMIN_SSH_KEY2}"
 
 # Vector Aggregator
-# set system syslog host 172.16.13.1 facility kern level 'warning'
-# set system syslog host 172.16.13.1 protocol 'tcp'
-# set system syslog host 172.16.13.1 port '6000'
-# set system syslog host 172.16.13.1 format 'octet-counted'
+# set system syslog host 10.11.10.2 facility kern level 'warning'
+# set system syslog host 10.11.10.2 protocol 'tcp'
+# set system syslog host 10.11.10.2 port '6000'
+# set system syslog host 10.11.10.2 format 'octet-counted'
