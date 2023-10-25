cdfbeab2f92feeac22848018447c15a7ec4e71c4
Eko Purwanto <eko.purwanto@monosense.io>
2023-10-25 11:36:29 +0700

    refactor(scripts): refactor apply-config.sh

--- bootstrap/nat.sh
+++ bootstrap/nat.sh
@@ -6,7 +6,7 @@ set interfaces pppoe pppoe0 authentication username "${SECRET_WAN_PPPOE_USER}"
 set interfaces pppoe pppoe0 authentication password "${SECRET_WAN_PPPOE_PASS}"
 set interfaces pppoe pppoe0 description 'WAN - PPPoE'
 set interfaces pppoe pppoe0 mtu '1500'
-set interfaces pppoe pppoe0 ip adjust-mss '1460'
+set interfaces pppoe pppoe0 ip adjust-mss clamp-mss-to-pmtu
 set interfaces pppoe pppoe0 source-interface 'bond0.1615'
 
 # ALL -> WAN masquerade
